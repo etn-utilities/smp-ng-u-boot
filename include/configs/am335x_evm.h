5a388838b9f9d2c80ecd1d2eef92bbd4a3008bd7
Scott Ellis <scott@jumpnowtek.com>
2017-05-14 09:46:03 -0400

    Remove unused boot options
    %% original patch: 0001-Remove-unused-boot-options.patch

--- include/configs/am335x_evm.h
+++ include/configs/am335x_evm.h
@@ -233,6 +233,7 @@
  * in memory.
  */
 
+#ifdef CONFIG_USB
 /*
  * USB configuration.  We enable MUSB support, both for host and for
  * gadget.  We set USB0 as peripheral and USB1 as host, based on the
@@ -245,6 +246,24 @@
 #define CONFIG_AM335X_USB0_MODE	MUSB_PERIPHERAL
 #define CONFIG_AM335X_USB1
 #define CONFIG_AM335X_USB1_MODE MUSB_HOST
+
+#ifndef CONFIG_SPL_USBETH_SUPPORT
+/* Fastboot */
+#define CONFIG_USB_FUNCTION_FASTBOOT
+#define CONFIG_CMD_FASTBOOT
+#define CONFIG_ANDROID_BOOT_IMAGE
+#define CONFIG_FASTBOOT_BUF_ADDR	CONFIG_SYS_LOAD_ADDR
+#define CONFIG_FASTBOOT_BUF_SIZE	0x07000000
+
+#define CONFIG_FASTBOOT_FLASH_MMC_DEV   1
+#endif
+
+#ifdef CONFIG_USB_MUSB_GADGET
+#define CONFIG_USB_ETHER
+#define CONFIG_USB_ETH_RNDIS
+#define CONFIG_USBNET_HOST_ADDR	"de:ad:be:af:00:00"
+#endif /* CONFIG_USB_MUSB_GADGET */
+#endif /* CONFIG_USB */
 
 /*
  * Disable MMC DM for SPL build and can be re-enabled after adding
