ef35dd4639d7466d787acee438cb980c8b9ecf3b
Ronald Landheer-Cieslak <ronaldlandheercieslak@eaton.com>
2017-10-13 07:59:25 -0400

    Add support for s25fl208kor_64k and s25fl208kor_4k flash
    %% original patch: 0004-Add-support-for-s25fl208kor_64k-and-s25fl208kor_4k-f.patch

--- drivers/mtd/spi/spi_flash_ids.c
+++ drivers/mtd/spi/spi_flash_ids.c
@@ -117,6 +117,8 @@ const struct spi_flash_info spi_flash_ids[] = {
 	{"s25fl512s_256k", INFO(0x010220, 0x4d00, 256 * 1024,   256, RD_FULL | WR_QPP) },
 	{"s25fl512s_64k",  INFO(0x010220, 0x4d01,  64 * 1024,  1024, RD_FULL | WR_QPP) },
 	{"s25fl512s_512k", INFO(0x010220, 0x4f00, 256 * 1024,   256, RD_FULL | WR_QPP) },
+	{"s25fl208kor_64k", INFO(0x014014, 0x0140,  64 * 1024,    16, RD_NORM) },
+	{"s25fl208kor_4k",  INFO(0x014014, 0x0140,   4 * 1024,   256, RD_NORM | SECT_4K) },
 #endif
 #ifdef CONFIG_SPI_FLASH_STMICRO		/* STMICRO */
 	{"m25p10",	   INFO(0x202011, 0x0, 32 * 1024,     4, 0) },
