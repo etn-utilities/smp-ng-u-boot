351a353a5c4735199c43e7aae760af890d9e710f
Ronald Landheer-Cieslak <ronaldlandheercieslak@eaton.com>
2018-01-02 10:08:31 -0500

    Configuration changes for NAND
    %% original patch: 0015-Configuration-changes-for-NAND.patch

--- include/configs/am335x_genepi.h
+++ include/configs/am335x_genepi.h
@@ -21,11 +21,35 @@
 
 #include <configs/ti_am335x_common.h>
 
+/* Nandflash timing definition */
+/* CONFIG 7 is at 0 because it is configured by the calling function*/
+#define M_NAND_GPMC_CONFIG1	0x00000800
+#define M_NAND_GPMC_CONFIG2	0x00050500
+#define M_NAND_GPMC_CONFIG3	0x00050501
+#define M_NAND_GPMC_CONFIG4	0x04000600
+#define M_NAND_GPMC_CONFIG5	0x00070909
+#define M_NAND_GPMC_CONFIG6	0x84000000
+#define M_NAND_GPMC_CONFIG7	0x00000F48
+
+/* polarity 1 = active low; 0 is active high */
+#define GPMC_WAIT1PIN_POLARITY 0
+#define GPMC_WAIT0PIN_POLARITY 1
+#define GPMC_WRITE_PROTECT_POLARITY 1
+#define GPMC_LIMITED_ADDRESS 1
+#define GPMC_NAND_FORCE_POSTED_WRITE 0
+
+
 #ifndef CONFIG_SPL_BUILD
+#define CONFIG_NAND
+#define CONFIG_NAND_OMAP_GPMC
 # define CONFIG_TIMESTAMP
 # define CONFIG_LZO
 #endif
 
+#define GPMC_CS0_FLASH				MTD_DEV_TYPE_NAND
+#define CONFIG_SYS_NAND_BASE		0x08000000
+#define CONFIG_SYS_NAND_SIZE		(256*1024*1024)
+#define CONFIG_SYS_MAX_NAND_DEVICE  1
 #define CONFIG_SPL_FRAMEWORK
 
 /* set max size to 128 MiB */
@@ -186,8 +210,8 @@
 #define CONFIG_SYS_NAND_ECCBYTES	14
 #define CONFIG_SYS_NAND_ONFI_DETECTION
 #define CONFIG_NAND_OMAP_ECCSCHEME	OMAP_ECC_BCH8_CODE_HW
-#define MTDIDS_DEFAULT			"nand0=nand.0"
-#define MTDPARTS_DEFAULT		"mtdparts=nand.0:" \
+/*#define MTDIDS_DEFAULT			"nand0=nand.0"*/
+/*#define MTDPARTS_DEFAULT		"mtdparts=nand.0:" \
 					"128k(NAND.SPL)," \
 					"128k(NAND.SPL.backup1)," \
 					"128k(NAND.SPL.backup2)," \
@@ -197,8 +221,7 @@
 					"128k(NAND.u-boot-env)," \
 					"128k(NAND.u-boot-env.backup1)," \
 					"8m(NAND.kernel)," \
-					"-(NAND.file-system)"
-#define CONFIG_SYS_NAND_BASE		0x00080000
+					"-(NAND.file-system)"*/
 #define CONFIG_SYS_NAND_U_BOOT_OFFS	0x000c0000
 /* NAND: SPL related configs */
 #ifdef CONFIG_SPL_NAND_SUPPORT
@@ -301,11 +324,11 @@
 #define CONFIG_ENV_SECT_SIZE		(4 << 10) /* 4 KB sectors */
 #define CONFIG_ENV_OFFSET		(768 << 10) /* 768 KiB in */
 #define CONFIG_ENV_OFFSET_REDUND	(896 << 10) /* 896 KiB in */
-#define MTDIDS_DEFAULT			"nor0=m25p80-flash.0"
-#define MTDPARTS_DEFAULT		"mtdparts=m25p80-flash.0:128k(SPL)," \
+/*#define MTDIDS_DEFAULT			"nor0=m25p80-flash.0"*/
+/*#define MTDPARTS_DEFAULT		"mtdparts=m25p80-flash.0:128k(SPL)," \
 					"512k(u-boot),128k(u-boot-env1)," \
 					"128k(u-boot-env2),3464k(kernel)," \
-					"-(rootfs)"
+					"-(rootfs)"*/
 #define CONFIG_SYS_NAND_U_BOOT_OFFS	 0x000c0000
 #elif defined(CONFIG_EMMC_BOOT)
 #define CONFIG_ENV_IS_IN_MMC
