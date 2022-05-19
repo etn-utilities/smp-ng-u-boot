9dc4434a3e70ea4b11d1eb52f2583916e9ea3748
Frederic Ouellet <fredericouellet@eaton.com>
2022-05-18 22:20:31 -0400

    WI #71865 - Implement boot counter logic
    Signed-off-by: Joleme Joinvil <jolemejoinvil@eaton.com>

--- board/variscite/imx8mm_var_dart/imx8mm_var_dart.h
+++ board/variscite/imx8mm_var_dart/imx8mm_var_dart.h
@@ -1,6 +1,8 @@
 int var_get_som_rev(struct var_eeprom *ep);
 int get_board_id(void);
 
+typedef struct eaton_boot_data_struct* smp_boot_data_ptr;
+
 enum {
 	DART_MX8M_MINI,
 	VAR_SOM_MX8M_MINI,
@@ -13,4 +15,24 @@ enum {
 	SOM_REV_12,
 	SOM_REV_13,
 	UNKNOWN_REV
+};
+
+enum {
+	DART_CARRIER_REV_1,
+	DART_CARRIER_REV_2,
+	DART_CARRIER_REV_UNDEF,
+};
+
+enum
+{
+	SMP_EMMC_SWITCH_PART_2_0,
+	SMP_EMMC_WRITE,
+	SMP_EMMC_READ,
+};
+
+enum
+{
+	SMP_BOOT_SD_CARD,
+	SMP_BOOT_INTERNAL_EMMC,
+	SMP_BOOT_UNKNOWN,
 };
